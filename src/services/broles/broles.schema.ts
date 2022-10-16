2806c661ba4646c82086c326361f6fc5c6b28146
Jamiu Adeniyi <asaajay775@gmail.com>
2022-10-16 15:48:11 +0000

    Update 16102022:1648

--- src/services/broles/broles.schema.ts
+++ src/services/broles/broles.schema.ts
@@ -11,7 +11,11 @@ export const brolesSchema = Type.Object(
     id: Type.Number(),
     businesId: Type.String(),
     shopId: Type.String(),
-    role: Type.Enum()
+    role: Type.Enum(
+      {
+       manager: 'manager',
+       assistance: 'assistance'
+      }),
   },
   { $id: 'Broles', additionalProperties: false }
 )
@@ -25,7 +29,7 @@ export const brolesExternalResolver = resolve<Broles, HookContext>({
 })
 
 // Schema for creating new entries
-export const brolesDataSchema = Type.Pick(brolesSchema, ['text'], {
+export const brolesDataSchema = Type.Omit(brolesSchema, ['id'], {
   $id: 'BrolesData',
   additionalProperties: false
 })
@@ -36,7 +40,7 @@ export const brolesDataResolver = resolve<Broles, HookContext>({
 })
 
 // Schema for allowed query properties
-export const brolesQueryProperties = Type.Pick(brolesSchema, ['id', 'text'], { additionalProperties: false })
+export const brolesQueryProperties = Type.Omit(brolesSchema, [], { additionalProperties: false })
 export const brolesQuerySchema = querySyntax(brolesQueryProperties)
 export type BrolesQuery = Static<typeof brolesQuerySchema>
 export const brolesQueryValidator = getValidator(brolesQuerySchema, queryValidator)
